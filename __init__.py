edafcdcdabc429b1cda66fe4e025b242b46341f4
flashton2003 <flashton@gmail.com>
2015-07-23 15:32:45 +0100

    added in run taxit

--- __init__.py
+++ __init__.py
@@ -12,8 +12,14 @@ class Package:
 
 	def parse_package(self, path_to_package):
 		for f in os.listdir(path_to_package):
-			if f.endswith(('.fa', '.fasta', '.fna')):
+			if f.endswith(('ref.fa', 'ref.fasta', 'ref.fna')):
 				self.ref_genome = os.path.join(path_to_package, f)
 			if f.endswith('var_list'):
 				self.var_list = os.path.join(path_to_package, f)
-		pass
+			if f.endswith('.log'):
+				self.fasttree_log = os.path.join(path_to_package, f)
+			if f.endswith('.var.fa'):
+				self.variant_fasta = os.path.join(path_to_package, f)
+			if f.endswith(('.tree', '.nwk')):
+				self.fasttree_tree = os.path.join(path_to_package, f)
+
